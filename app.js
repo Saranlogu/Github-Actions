75fe987ab951b02d28a2ebb70412db1f77a83809
Saranlogu <saranlogu13@gmail.com>
2025-12-01 15:22:42 +0530

    Added new function

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-console.log("Hello, Saranya");
-
 import moment from "moment";
-let date = moment().format('LL');
-console.log("Today's date is: " + date);
+
+function greetUser(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+function showDateTime() {
+    const date = moment().format('LL');
+    const time = moment().format('LTS');
+    const day = moment().format('dddd');
+    console.log(`Today's date is: ${date}`);
+    console.log(`Current time is: ${time}`);
+    console.log(`It's a great ${day}!`);
+}
+
+// Run the functions
+greetUser("Saranya");
+showDateTime();
